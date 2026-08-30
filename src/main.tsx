658b1b3d458818fff481e76a790d7db79242e6c6
Kaira adira rahayu <KAIRAKAY22@GMAIL.COM>
2026-08-30 16:20:12 +0700

    Keep admin and driver on separate operations URLs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CustomerLayout from "../app/layout";
+import OperationsHome from "../app/page";
+import "../app/globals.css";
 import "../styles/customer.css";
 import "./realtime";
 
+const view=new URLSearchParams(location.search).get("view");
+const operations=view==="admin"||view==="driver";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
- <React.StrictMode><CustomerLayout/></React.StrictMode>,
+ <React.StrictMode>{operations?<OperationsHome/>:<CustomerLayout/>}</React.StrictMode>,
 );
